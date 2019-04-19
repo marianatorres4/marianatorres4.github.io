f5a5c3aa09d23592c66427068f317822c77e8d76
Julius Garbe <julius.garbe@pik-potsdam.de>
2019-04-19 21:01:08 +0200

    Update talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,22 +1,12 @@
 var addressPoints = [
   [
-    "Berkeley CA, USA",
-    37.8708393,
-    -122.2728638
-  ],
+    "Potsdam, Germany", 
+    52.4009309, 
+    13.0591397
+  ], 
   [
-    "London, UK",
-    51.5073219,
-    -0.1276473
-  ],
-  [
-    "San Francisco, California",
-    37.7792808,
-    -122.4192362
-  ],
-  [
-    "Los Angeles, CA",
-    34.0543942,
-    -118.2439408
+    "Vienna, Austria", 
+    48.2083537, 
+    16.3725042
   ]
 ];
