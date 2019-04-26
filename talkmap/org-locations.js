d39b0590b4a615af7c81349abe88312bceedf1e6
Julius Garbe <julius.garbe@pik-potsdam.de>
2019-04-26 16:49:02 +0200

    Add details in talk map

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,12 +1,22 @@
 var addressPoints = [
   [
-    "Potsdam, Germany", 
-    52.4009309, 
-    13.0591397
-  ], 
-  [
-    "Vienna, Austria", 
-    48.2083537, 
+    "Hysteresis of the Antarctic Ice Sheet | EGU General Assembly 2019, Vienna, Austria",
+    48.2083537,
     16.3725042
+  ],
+  [
+    "Hysteresis behaviour of the Antarctic Ice Sheet with the Parallel Ice Sheet Model | EGU General Assembly 2018, Vienna, Austria",
+    48.2083537,
+    16.3725042
+  ],
+  [
+    "Tipping of the Antarctic Ice Sheet influenced by Greenland ice loss | Potsdam Institute for Climate Impact Research, Potsdam, Germany",
+    52.4009309,
+    13.0591397
+  ],
+  [
+    "Hysteresis Behavior of the Antarctic Ice Sheet | Potsdam Institute for Climate Impact Research, Potsdam, Germany",
+    52.4009309,
+    13.0591397
   ]
 ];
