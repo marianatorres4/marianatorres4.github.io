67c6728d79d93ef635a9a5025fd773f79018b1f5
Julius Garbe <julius.garbe@pik-potsdam.de>
2019-07-20 12:34:28 +0200

    Fix directory paths for talkmap

--- talkmap/org-locations.js
+++ talkmap/org-locations.js
@@ -1,6 +1,6 @@
 var addressPoints = [
   [
-    "Hysteresis of the Antarctic Ice Sheet | EGU General Assembly 2019, Vienna, Austria",
+    "Projecting Antarctica's contribution to future sea level rise from basal ice-shelf melt using linear response functions of 16 ice sheet models (LARMIP-2) | EGU General Assembly 2019, Vienna, Austria",
     48.2083537,
     16.3725042
   ],
@@ -18,5 +18,10 @@ var addressPoints = [
     "Hysteresis Behavior of the Antarctic Ice Sheet | Potsdam Institute for Climate Impact Research, Potsdam, Germany",
     52.4009309,
     13.0591397
+  ],
+  [
+    "Hysteresis of the Antarctic Ice Sheet | EGU General Assembly 2019, Vienna, Austria",
+    48.2083537,
+    16.3725042
   ]
 ];
