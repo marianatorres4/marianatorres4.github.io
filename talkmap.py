695c76a20a62e50f88eb083d07cdb79c9dd77c26
Julius Garbe <julius.garbe@pik-potsdam.de>
2021-12-17 14:04:43 +0100

    Update talkmap scripts

--- talkmap.py
+++ talkmap.py
@@ -1,4 +1,4 @@
-
+#!/usr/bin/env python
 
 # # Leaflet cluster map of talk locations
 # 
@@ -9,20 +9,22 @@
 # Requires: glob, getorg, geopy
 #!pip install getorg python-frontmatter
 
+import os
 import glob
 import getorg
+import shutil
 from geopy import Nominatim
 
 g = glob.glob("_talks/*.md")
 
-geocoder = Nominatim(user_agent="my-application")
+geocoder = Nominatim(user_agent="my-application1") # small hack, as `user_agent="my-application"` (default) returns an error
 location_dict = {}
 location = ""
 permalink = ""
 title = ""
 venue = ""
-count=0
 
+count=0
 for file in g:
     with open(file, 'r') as f:
         lines = f.read()
@@ -44,9 +46,23 @@ for file in g:
 
         key = str(title + " | " + venue + ", " + location)
 
-        location_dict[key] = geocoder.geocode(location)
-        print(key, "\n", location_dict[key])
-    count = count + 1
+        if location.endswith("(online)"): # special case for online talks
+            location = location.rstrip(" (online)")
+        
+        location_dict[key] = geocoder.geocode(location, timeout=60)
+        
+        print(key, "\n", location_dict[key], "\n")
+    
+    count += 1
+print(count, "talks")
+
+# remove old org-locations.js first, otherwise changed entries just get appended
+os.remove("talkmap/org-locations.js")
 
 m = getorg.orgmap.create_map_obj()
 getorg.orgmap.output_html_cluster_map(location_dict, folder_name="talkmap/", hashed_usernames=False)
+
+# some files include manual changes but are overwitten by getorg, like map.html and leaflet_dist/screen.css
+# thus a backup version is kept in restore/ and copied to replace the newly created ones
+print('copying files from restore/ directory')
+shutil.copytree("./talkmap/restore/","./talkmap/", dirs_exist_ok=True)
